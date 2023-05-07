3fe7668edd4f85c8ca5846883897d97dd3815017
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-05-07 12:30:39 -0300

    chore: limpando os icons contidos na pasta `public`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,17 @@
+import React from 'react'
+
 export const metadata = {
   title: 'Programou',
   description: 'Plataforma de estudo de tecnologia FrontEnd, Bakcend e Mobile',
 }
 
-export default function RootLayout({children,}: { children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>{children}</body>
     </html>
   )
