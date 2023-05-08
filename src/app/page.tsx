2a72f0fa75d289d688480afc63052bb76d1dd5e2
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-05-07 22:35:07 -0300

    style: colocando as cores no simbolos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Footer } from 'programou/components/Footer'
+import { Logo } from 'programou/components/Logo'
 
+import { AiFillNotification } from 'react-icons/ai'
 import { BsGithub, BsYoutube } from 'react-icons/bs'
 
 export default function Home() {
@@ -8,18 +10,17 @@ export default function Home() {
       <div className="h-screen grid max-w-[1080px] m-auto">
         <div />
 
-        <div className="text-center mx-auto">
-          <h1 className="font-bold text-4xl text-turquoise-500">
-            &lt; Programou /&gt;
-          </h1>
+        <div className="font-bold text-center mx-auto p-4">
+          <Logo />
 
-          <h2 className="font-bold mt-4 font-mono">
+          <h2 className="mt-4 font-mono">
             Onde suas ideias se transformam em códigos poderosos!
           </h2>
         </div>
 
         <div className="mx-4">
-          <div className="text-center gap-3 bg-gray-900 rounded-lg p-4">
+          <div className="text-center gap-3 bg-gray-900 rounded-lg p-4 md:flex md:items-center md:justify-center ">
+            <AiFillNotification className="m-auto min-w-[32px]" size="32px" />
             <p className="text-justify m-2">
               O Nosso site está nesse momento em desenvolvimento para poder
               atender as necessidades dos usuários da melhor forma possível. Nos
@@ -30,10 +31,14 @@ export default function Home() {
         </div>
 
         <div className="mb-4 mx-auto flex items-center justify-center gap-4">
-          <a href="https://www.youtube.com/@programou">
+          <a
+            href="https://www.youtube.com/@programou"
+            target="_blank"
+            rel="noreferrer"
+          >
             <BsYoutube size={32} />
           </a>
-          <a href="#https://github.com/programou">
+          <a href="#https://github.com/programou" target="_blank">
             <BsGithub size={32} />
           </a>
         </div>
---
+++ src/components/Logo.tsx
@@ -0,0 +1,9 @@
+export function Logo() {
+  return (
+    <h1 className="text-4xl">
+      <span className="text-turquoise-500">&lt;&nbsp;</span>
+      Programou
+      <span className="text-turquoise-500">&nbsp;/&gt;</span>
+    </h1>
+  )
+}
