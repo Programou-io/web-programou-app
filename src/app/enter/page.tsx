0e693a267b27c48bc3ab1190497927b9292584ee
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-05-12 16:15:10 -0300

    refactor: extraindo os micro componentes do login

--- src/app/enter/page.tsx
+++ src/app/enter/page.tsx
@@ -21,51 +21,74 @@ export default function EnterPage() {
     router.push('/hub')
   }
 
+  function makeBoxHeaderSection() {
+    return (
+      <div className="px-4">
+        <button onClick={backActionHandler}>
+          <BsArrowLeft size={24} />
+        </button>
+        <Heading className="text-2xl">Entre</Heading>
+        <Text className="text-gray-400">Faça login e comece a usar!</Text>
+      </div>
+    )
+  }
+
+  function makeTextFieldsSection() {
+    return (
+      <div className="grid gap-4">
+        <TextField prefix="@" label="Nome de usuario" />
+        <TextField label="Senha" />
+      </div>
+    )
+  }
+
+  function makeCheckboxSection() {
+    return (
+      <div className="flex items-center gap-2">
+        <Checkbox />
+        <Text>Lembre-se de min</Text>
+      </div>
+    )
+  }
+
+  function makeLinksSection() {
+    return (
+      <div className="text-center grid gap-2">
+        <Text>
+          <a className="underline" href="/recuperar-conta">
+            Esqueceu sua senha?
+          </a>
+        </Text>
+        <Text>
+          Não possui conta?{' '}
+          <a
+            className="underline font-bold text-turquoise-500"
+            href="/register"
+          >
+            Crie uma agora!
+          </a>
+        </Text>
+      </div>
+    )
+  }
+
   return (
     <div className="flex items-center justify-center">
       <div className="w-[550px] mt-36">
-        <div className="px-4">
-          <button onClick={backActionHandler}>
-            <BsArrowLeft size={24} />
-          </button>
-          <Heading className="text-2xl">Entre</Heading>
-          <Text className="text-gray-400">Faça login e comece a usar!</Text>
-        </div>
+        {makeBoxHeaderSection()}
 
         <Box className="mt-5">
           <Logo className="flex items-center justify-center font-bold text-2xl" />
 
           <div className="grid gap-4">
-            <div className="grid gap-4">
-              <TextField prefix="@" label="Nome de usuario" />
-              <TextField label="Senha" />
-            </div>
-
-            <div className="flex items-center gap-2">
-              <Checkbox />
-              <Text>Lembre-se de min</Text>
-            </div>
+            {makeTextFieldsSection()}
+            {makeCheckboxSection()}
 
             <PrimaryButton onClick={authenticateActionHandler}>
               Entrar
             </PrimaryButton>
 
-            <div className="text-center grid gap-2">
-              <Text>
-                <a className="underline" href="/recuperar-conta">
-                  Esqueceu sua senha?
-                </a>
-              </Text>
-              <Text>
-                Não possui conta?{' '}
-                <a
-                  className="underline font-bold text-turquoise-500"
-                  href="/register"
-                >
-                  Crie uma agora!
-                </a>
-              </Text>
-            </div>
+            {makeLinksSection()}
           </div>
         </Box>
       </div>
