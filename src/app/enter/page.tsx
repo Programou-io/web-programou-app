9269f2db07ba0ac8b31b435f860682b412af4183
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2023-05-12 17:19:57 -0300

    feat: add modal para recuperar senha

--- src/app/enter/page.tsx
+++ src/app/enter/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import * as Dialog from '@radix-ui/react-dialog'
 import { useRouter } from 'next/navigation'
 import { Box } from 'programou/components/Box'
 import { Checkbox } from 'programou/components/Checkbox'
@@ -9,6 +10,7 @@ import { PrimaryButton } from 'programou/components/PrimaryButton'
 import { Text } from 'programou/components/Text'
 import { TextField } from 'programou/components/TextField'
 import { BsArrowLeft } from 'react-icons/bs'
+import { IoMdClose } from 'react-icons/io'
 
 export default function EnterPage() {
   const router = useRouter()
@@ -19,6 +21,38 @@ export default function EnterPage() {
 
   function authenticateActionHandler() {
     router.push('/hub')
+  }
+
+  function recoveryPasswordActionHandler() {}
+
+  function makeDialogComponent() {
+    return (
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black opacity-80 fixed inset-0 ease-in-out duration-[0.2s]" />
+        <Dialog.Content className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          <Box>
+            <Dialog.DialogClose asChild>
+              <IoMdClose size={32} className="cursor-pointer" />
+            </Dialog.DialogClose>
+            <Dialog.Title className="text-white text-2xl font-bold">
+              Recupere sua senha
+            </Dialog.Title>
+            <Dialog.Description className="text-gray-200 text-lg">
+              Caso tenha perdido seu acesso você pode recupera-lo com seu email
+              informado no momento da criação da conta.
+            </Dialog.Description>
+
+            <TextField label="E-mail" placeholder="username@email.com" />
+
+            <Dialog.DialogClose asChild>
+              <PrimaryButton onClick={recoveryPasswordActionHandler}>
+                Recuperar
+              </PrimaryButton>
+            </Dialog.DialogClose>
+          </Box>
+        </Dialog.Content>
+      </Dialog.Portal>
+    )
   }
 
   function makeBoxHeaderSection() {
@@ -51,24 +85,33 @@ export default function EnterPage() {
     )
   }
 
+  function makeRecoveryPasswordSection() {
+    return (
+      <>
+        <Dialog.Trigger asChild>
+          <button className="underline">Esqueceu sua senha?</button>
+        </Dialog.Trigger>
+        {makeDialogComponent()}
+      </>
+    )
+  }
+
   function makeLinksSection() {
     return (
-      <div className="text-center grid gap-2">
-        <Text>
-          <a className="underline" href="/recuperar-conta">
-            Esqueceu sua senha?
-          </a>
-        </Text>
-        <Text>
-          Não possui conta?{' '}
-          <a
-            className="underline font-bold text-turquoise-500"
-            href="/register"
-          >
-            Crie uma agora!
-          </a>
-        </Text>
-      </div>
+      <Dialog.Root>
+        <div className="text-center grid gap-2">
+          {makeRecoveryPasswordSection()}
+          <Text>
+            Não possui conta?{' '}
+            <a
+              className="underline font-bold text-turquoise-500"
+              href="/register"
+            >
+              Crie uma agora!
+            </a>
+          </Text>
+        </div>
+      </Dialog.Root>
     )
   }
 
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,16 +1,17 @@
+import { HTMLAttributes } from 'react'
 import { Text } from './Text'
 import { TextInput } from './TextInput'
 
-interface TextFieldProps {
+interface TextFieldProps extends HTMLAttributes<HTMLInputElement> {
   label: string
   prefix?: string
 }
 
-export function TextField(props: TextFieldProps) {
+export function TextField({ prefix, ...props }: TextFieldProps) {
   return (
     <div className="grid gap-2">
       <Text>{props.label}</Text>
-      <TextInput prefix={props.prefix} />
+      <TextInput prefix={prefix} {...props} />
     </div>
   )
 }
